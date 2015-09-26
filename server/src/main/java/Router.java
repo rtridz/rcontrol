352f841801f6ffbb030d1894182af37775fc55d1
Larionov Vadim <larionov.vadim@mail.ru>
2015-09-27 02:03:50 +0300

    Сервер с использованием рефлексии

---
+++ server/src/main/java/Router.java
@@ -0,0 +1,38 @@
+import Routes.Category;
+import Routes.Handler;
+import Routes.Hello;
+import com.sun.istack.internal.Nullable;
+
+import java.util.HashMap;
+
+/**
+ * Created by vadim on 26.09.15.
+ */
+public class Router {
+    private HashMap<String, String> routes; // url, handlerClassName
+
+    public Router() {
+        routes = new HashMap<>();
+        // TODO Hard Code
+        addRoute("/category", Category.class.getName());
+        addRoute("/hello", Hello.class.getName());
+    }
+
+    @Nullable
+    public Handler getHandler(String uri) {
+        String handlerClassName = routes.get(uri);
+        Handler handler = null;
+        if (handlerClassName != null) {
+            try {
+                handler = (Handler) Class.forName(handlerClassName).newInstance();
+            } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return handler;
+    }
+
+    public void addRoute(String url, String handlerClassName) {
+        routes.put(url, handlerClassName);
+    }
+}
---
+++ server/src/main/java/Server.java
@@ -0,0 +1,38 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * Created by vadim on 18.09.15.
+ */
+public class Server {
+    private String host;
+    private int port;
+
+    public Server(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void run() throws InterruptedException {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ServerInitializer())
+                    .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+                    .childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
+
+            ChannelFuture future = b.bind(host, port);
+            future.channel().closeFuture().sync();
+        }
+        finally {
+            group.shutdownGracefully();
+        }
+    }
+}
